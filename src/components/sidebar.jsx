96fb93f0bf6f1af8075aa1ee7a4194fde53b88cc
nordahn87 <charleynordahn87@gmail.com>
2022-07-07 20:23:55 +0200

    Fixed Sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -31,41 +31,44 @@ const SideBar = (props) => {
     }, [props.id])
 
     return (
-        <section
-            id="Sidebar" className={(props.sidebar ? 'right-0' : 'right-[-100vw] md:right-[-50vw]') + ' fixed bg-white h-full top-0 w-[100vw] md:w-[50vw] transition-all p-12'}>
-            <button
-                className="bg-gray-300 w-[40px] rounded-full h-[40px] text-xl mb-8"
-                onClick={props.toogleSidebar}>
-                &#10005;
-            </button>
-            <section className="mb-6">
-                <h2 className="text-5xl font-bold">{beer.name}</h2>
-                <h3 className="text-2xl mb-6">{beer.tagline}</h3>
-                <img className="beer-img" src={beer.image_url} alt="A beer"></img>
+        <>
+            <section
+                className={(props.sidebar ? 'right-0' : 'right-[-100vw] md:right-[-50vw]') + ' fixed bg-white h-full top-0 w-[100vw] md:w-[50vw] transition-all p-12 z-50'}>
+                <button
+                    className="bg-gray-300 w-[40px] rounded-full h-[40px] text-xl mb-8"
+                    onClick={props.toogleSidebar}>
+                    &#10005;
+                </button>
+                <section className="mb-6">
+                    <h2 className="text-5xl font-bold">{beer.name}</h2>
+                    <h3 className="text-2xl mb-6">{beer.tagline}</h3>
+                    <img className="beer-img" src={beer.image_url} alt="A beer"></img>
+                </section>
+                <ul>
+                    <li className="flex">
+                        <h3 className="mr-3 font-bold">ABV:</h3>
+                        <p>Alc {beer.abv}% VOL</p>
+                    </li>
+                    <li className="flex">
+                        <h3 className="mr-3 font-bold">First brewed:</h3>
+                        <p>{beer.first_brewed}</p>
+                    </li>
+                    <li className="flex">
+                        <h3 className="mr-3 font-bold">Boil volume:</h3>
+                        <p>{beer.boil_volume.value} {beer.boil_volume.unit}</p>
+                    </li>
+                    <li>
+                        <hr />
+                    </li>
+                    <li>
+                        <h3 className="mr-3 mb-4 font-bold text-xl">Description</h3>
+                        <p className="bg-gray-100 p-6">{beer.description}</p>
+                    </li>
+                </ul>
+                <Comments />
             </section>
-            <ul>
-                <li className="flex">
-                    <h3 className="mr-3 font-bold">ABV:</h3>
-                    <p>Alc {beer.abv}% VOL</p>
-                </li>
-                <li className="flex">
-                    <h3 className="mr-3 font-bold">First brewed:</h3>
-                    <p>{beer.first_brewed}</p>
-                </li>
-                <li className="flex">
-                    <h3 className="mr-3 font-bold">Boil volume:</h3>
-                    <p>{beer.boil_volume.value} {beer.boil_volume.unit}</p>
-                </li>
-                <li>
-                    <hr />
-                </li>
-                <li>
-                    <h3 className="mr-3 mb-4 font-bold text-xl">Description</h3>
-                    <p className="bg-gray-100 p-6">{beer.description}</p>
-                </li>
-            </ul>
-            <Comments />
-        </section>
+            <span className={(props.sidebar ? 'opacity-50' : 'hidden') + ' fixed top-0 left-0 bg-black w-screen h-screen transition-all'}></span>
+        </>
     )
 }
 
